4cf994769bf669ba3d40b63587e946965b383a72
Kingerbans <ducminhngo95@gmail.com>
2021-09-14 23:38:21 +0700

    :+1:
    :+1:

--- Client/app/src/main/java/com/example/webrtcdemo/CallActivity.java
+++ Client/app/src/main/java/com/example/webrtcdemo/CallActivity.java
@@ -1,229 +1,96 @@
 package com.example.webrtcdemo;
 
-import androidx.annotation.NonNull;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.CaptureRequest;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
-import android.view.Surface;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.widget.Toast;
+import org.webrtc.AudioSource;
+import org.webrtc.AudioTrack;
+import org.webrtc.Camera1Enumerator;
+import org.webrtc.EglBase;
+import org.webrtc.MediaConstraints;
+import org.webrtc.PeerConnectionFactory;
+import org.webrtc.SurfaceTextureHelper;
+import org.webrtc.SurfaceViewRenderer;
+import org.webrtc.VideoCapturer;
+import org.webrtc.VideoSource;
+import org.webrtc.VideoTrack;
 
-import com.example.webrtcdemo.Handler.SocketHandler;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class CallActivity extends AppCompatActivity implements SurfaceHolder.Callback, Handler.Callback{
-    SocketHandler socketHandler;
-    static final String TAG = "CamTest";
-    static final int MY_PERMISSIONS_REQUEST_CAMERA = 1242;
-    private static final int MSG_CAMERA_OPENED = 1;
-    private static final int MSG_SURFACE_READY = 2;
-    private final Handler mHandler = new Handler(this);
-    SurfaceView mSurfaceView;
-    SurfaceHolder mSurfaceHolder;
-    CameraManager mCameraManager;
-    String[] mCameraIDsList;
-    CameraDevice.StateCallback mCameraStateCB;
-    CameraDevice mCameraDevice;
-    CameraCaptureSession mCaptureSession;
-    boolean mSurfaceCreated = true;
-    boolean mIsCameraConfigured = false;
-    private Surface mCameraSurface = null;
+public class CallActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_call);
+        setContentView(R.layout.activity_main);
 
-        this.mSurfaceView = (SurfaceView) findViewById(R.id.local_surface_view);
-        this.mSurfaceHolder = this.mSurfaceView.getHolder();
-        this.mSurfaceHolder.addCallback(this);
-        this.mCameraManager = (CameraManager) this.getSystemService(Context.CAMERA_SERVICE);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA},
+                    50); }
 
-        try {
-            mCameraIDsList = this.mCameraManager.getCameraIdList();
-            for (String id : mCameraIDsList) {
-                Log.v(TAG, "CameraID: " + id);
-            }
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
 
-        mCameraStateCB = new CameraDevice.StateCallback() {
-            @Override
-            public void onOpened(CameraDevice camera) {
-                Toast.makeText(getApplicationContext(), "onOpened", Toast.LENGTH_SHORT).show();
+        // create PeerConnectionFactory
+        PeerConnectionFactory.InitializationOptions initializationOptions = PeerConnectionFactory.InitializationOptions.builder(this).createInitializationOptions();
+        PeerConnectionFactory.initialize(initializationOptions);
+        PeerConnectionFactory peerConnectionFactory = PeerConnectionFactory.builder().createPeerConnectionFactory();
 
-                mCameraDevice = camera;
-                mHandler.sendEmptyMessage(MSG_CAMERA_OPENED);
-            }
+        // create AudioSource
+        AudioSource audioSource = peerConnectionFactory.createAudioSource(new MediaConstraints());
+        AudioTrack audioTrack = peerConnectionFactory.createAudioTrack("101", audioSource);
 
-            @Override
-            public void onDisconnected(CameraDevice camera) {
-                Toast.makeText(getApplicationContext(), "onDisconnected", Toast.LENGTH_SHORT).show();
-            }
+        EglBase.Context eglBaseContext = EglBase.create().getEglBaseContext();
 
-            @Override
-            public void onError(CameraDevice camera, int error) {
-                Toast.makeText(getApplicationContext(), "onError", Toast.LENGTH_SHORT).show();
-            }
-        };
+        SurfaceTextureHelper surfaceTextureHelper = SurfaceTextureHelper.create("CaptureThread", eglBaseContext);
+
+        // create VideoCapturer
+        VideoCapturer videoCapturer = createCameraCapturer();
+        VideoSource videoSource = peerConnectionFactory.createVideoSource(videoCapturer.isScreencast());
+        videoCapturer.initialize(surfaceTextureHelper, getApplicationContext(), videoSource.getCapturerObserver());
+        videoCapturer.startCapture(480, 640, 30);
+
+        SurfaceViewRenderer localView = findViewById(R.id.localView);
+        localView.setMirror(true);   // Sao cái này nó không set đc <.....................>
+        localView.init(eglBaseContext, null);
+        localView.setVisibility(View.VISIBLE);
+
+        // create VideoTrack
+        VideoTrack videoTrack = peerConnectionFactory.createVideoTrack("100", videoSource);
+        // display in localView
+        videoTrack.addSink(localView);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    private VideoCapturer createCameraCapturer() {
+        Camera1Enumerator enumerator = new Camera1Enumerator(false);
+        final String[] deviceNames = enumerator.getDeviceNames();
 
-        //requesting permission
-        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
-        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+        // First, try to find front facing camera
+        for (String deviceName : deviceNames) {
+            if (enumerator.isFrontFacing(deviceName)) {
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
 
-            } else {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
-                Toast.makeText(getApplicationContext(), "request permission", Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            Toast.makeText(getApplicationContext(), "PERMISSION_ALREADY_GRANTED", Toast.LENGTH_SHORT).show();
-            try {
-                mCameraManager.openCamera(mCameraIDsList[1], mCameraStateCB, new Handler());
-            } catch (CameraAccessException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        try {
-            if (mCaptureSession != null) {
-                mCaptureSession.stopRepeating();
-                mCaptureSession.close();
-                mCaptureSession = null;
-            }
-
-            mIsCameraConfigured = false;
-        } catch (final CameraAccessException e) {
-            // Doesn't matter, cloising device anyway
-            e.printStackTrace();
-        } catch (final IllegalStateException e2) {
-            // Doesn't matter, cloising device anyway
-            e2.printStackTrace();
-        } finally {
-            if (mCameraDevice != null) {
-                mCameraDevice.close();
-                mCameraDevice = null;
-                mCaptureSession = null;
-            }
-        }
-    }
-
-    @Override
-    public boolean handleMessage(Message msg) {
-        switch (msg.what) {
-            case MSG_CAMERA_OPENED:
-            case MSG_SURFACE_READY:
-                // if both surface is created and camera device is opened
-                // - ready to set up preview and other things
-                if (mSurfaceCreated && (mCameraDevice != null)
-                        && !mIsCameraConfigured) {
-                    configureCamera();
+                if (videoCapturer != null) {
+                    return videoCapturer;
                 }
-                break;
-        }
-
-        return true;
-    }
-
-    private void configureCamera() {
-        // prepare list of surfaces to be used in capture requests
-        List<Surface> sfl = new ArrayList<Surface>();
-
-        sfl.add(mCameraSurface); // surface for viewfinder preview
-
-        // configure camera with all the surfaces to be ever used
-        try {
-            mCameraDevice.createCaptureSession(sfl,
-                    new CaptureSessionListener(), null);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-
-        mIsCameraConfigured = true;
-    }
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_CAMERA:
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
-                    try {
-                        mCameraManager.openCamera(mCameraIDsList[1], mCameraStateCB, new Handler());
-                    } catch (CameraAccessException e) {
-                        e.printStackTrace();
-                    }
-                break;
-        }
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        mCameraSurface = holder.getSurface();
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        mCameraSurface = holder.getSurface();
-        mSurfaceCreated = true;
-        mHandler.sendEmptyMessage(MSG_SURFACE_READY);
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        mSurfaceCreated = false;
-    }
-
-    private class CaptureSessionListener extends
-            CameraCaptureSession.StateCallback {
-        @Override
-        public void onConfigureFailed(final CameraCaptureSession session) {
-            Log.d(TAG, "CaptureSessionConfigure failed");
-        }
-
-        @Override
-        public void onConfigured(final CameraCaptureSession session) {
-            Log.d(TAG, "CaptureSessionConfigure onConfigured");
-            mCaptureSession = session;
-
-            try {
-                CaptureRequest.Builder previewRequestBuilder = mCameraDevice
-                        .createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-                previewRequestBuilder.addTarget(mCameraSurface);
-                mCaptureSession.setRepeatingRequest(previewRequestBuilder.build(),
-                        null, null);
-            } catch (CameraAccessException e) {
-                Log.d(TAG, "setting up preview failed");
-                e.printStackTrace();
             }
         }
+
+        // Front facing camera not found, try something else
+        for (String deviceName : deviceNames) {
+            if (!enumerator.isFrontFacing(deviceName)) {
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+
+                if (videoCapturer != null) {
+                    return videoCapturer;
+                }
+            }
+        }
+
+        return null;
     }
+
 }
