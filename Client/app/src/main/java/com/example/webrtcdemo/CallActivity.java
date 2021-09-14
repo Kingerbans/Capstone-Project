be961067597d2c99be527d8f63f79c2ad604d42c
Kingerbans <ducminhngo95@gmail.com>
2021-09-15 02:17:10 +0700

    a
    a

--- Client/app/src/main/java/com/example/webrtcdemo/CallActivity.java
+++ Client/app/src/main/java/com/example/webrtcdemo/CallActivity.java
@@ -1,19 +1,35 @@
 package com.example.webrtcdemo;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.view.View;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import org.webrtc.AudioSource;
-import org.webrtc.AudioTrack;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import com.example.webrtcdemo.Handler.SocketHandler;
+
 import org.webrtc.Camera1Enumerator;
+import org.webrtc.DefaultVideoDecoderFactory;
+import org.webrtc.DefaultVideoEncoderFactory;
 import org.webrtc.EglBase;
-import org.webrtc.MediaConstraints;
+import org.webrtc.MediaStream;
+import org.webrtc.PeerConnection;
 import org.webrtc.PeerConnectionFactory;
 import org.webrtc.SurfaceTextureHelper;
 import org.webrtc.SurfaceViewRenderer;
@@ -21,67 +37,64 @@ import org.webrtc.VideoCapturer;
 import org.webrtc.VideoSource;
 import org.webrtc.VideoTrack;
 
-public class CallActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CallActivity extends AppCompatActivity{
+    PeerConnectionFactory peerConnectionFactory;
+    PeerConnection peerConnectionLocal;
+    PeerConnection peerConnectionRemote;
+    SurfaceViewRenderer localView;
+    SurfaceViewRenderer remoteView;
+    MediaStream mediaStreamLocal;
+    MediaStream mediaStreamRemote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) !=
-                PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA},
-                    50); }
-
-
-        // create PeerConnectionFactory
-        PeerConnectionFactory.InitializationOptions initializationOptions = PeerConnectionFactory.InitializationOptions.builder(this).createInitializationOptions();
-        PeerConnectionFactory.initialize(initializationOptions);
-        PeerConnectionFactory peerConnectionFactory = PeerConnectionFactory.builder().createPeerConnectionFactory();
-
-        // create AudioSource
-        AudioSource audioSource = peerConnectionFactory.createAudioSource(new MediaConstraints());
-        AudioTrack audioTrack = peerConnectionFactory.createAudioTrack("101", audioSource);
+        setContentView(R.layout.activity_call);
 
         EglBase.Context eglBaseContext = EglBase.create().getEglBaseContext();
 
-        SurfaceTextureHelper surfaceTextureHelper = SurfaceTextureHelper.create("CaptureThread", eglBaseContext);
+        // create PeerConnectionFactory
+        PeerConnectionFactory.initialize(PeerConnectionFactory.InitializationOptions
+                .builder(this)
+                .createInitializationOptions());
+        PeerConnectionFactory.Options options = new PeerConnectionFactory.Options();
+        DefaultVideoEncoderFactory defaultVideoEncoderFactory =
+                new DefaultVideoEncoderFactory(eglBaseContext, true, true);
+        DefaultVideoDecoderFactory defaultVideoDecoderFactory =
+                new DefaultVideoDecoderFactory(eglBaseContext);
+        peerConnectionFactory = PeerConnectionFactory.builder()
+                .setOptions(options)
+                .setVideoEncoderFactory(defaultVideoEncoderFactory)
+                .setVideoDecoderFactory(defaultVideoDecoderFactory)
+                .createPeerConnectionFactory();
 
+        SurfaceTextureHelper surfaceTextureHelper = SurfaceTextureHelper.create("CaptureThread", eglBaseContext);
         // create VideoCapturer
-        VideoCapturer videoCapturer = createCameraCapturer();
+        VideoCapturer videoCapturer = createCameraCapturer(true);
         VideoSource videoSource = peerConnectionFactory.createVideoSource(videoCapturer.isScreencast());
         videoCapturer.initialize(surfaceTextureHelper, getApplicationContext(), videoSource.getCapturerObserver());
         videoCapturer.startCapture(480, 640, 30);
 
-        SurfaceViewRenderer localView = findViewById(R.id.localView);
-        localView.setMirror(true);   // Sao cái này nó không set đc <.....................>
+        localView = findViewById(R.id.localView);
+        localView.setMirror(true);
         localView.init(eglBaseContext, null);
-        localView.setVisibility(View.VISIBLE);
 
         // create VideoTrack
         VideoTrack videoTrack = peerConnectionFactory.createVideoTrack("100", videoSource);
-        // display in localView
-        videoTrack.addSink(localView);
+//        // display in localView
+//        videoTrack.addSink(localView);
     }
 
-    private VideoCapturer createCameraCapturer() {
+    private VideoCapturer createCameraCapturer(boolean isFront) {
         Camera1Enumerator enumerator = new Camera1Enumerator(false);
         final String[] deviceNames = enumerator.getDeviceNames();
 
         // First, try to find front facing camera
         for (String deviceName : deviceNames) {
-            if (enumerator.isFrontFacing(deviceName)) {
-                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
-
-                if (videoCapturer != null) {
-                    return videoCapturer;
-                }
-            }
-        }
-
-        // Front facing camera not found, try something else
-        for (String deviceName : deviceNames) {
-            if (!enumerator.isFrontFacing(deviceName)) {
+            if (isFront ? enumerator.isFrontFacing(deviceName) : enumerator.isBackFacing(deviceName)) {
                 VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
 
                 if (videoCapturer != null) {
@@ -92,5 +105,4 @@ public class CallActivity extends AppCompatActivity {
 
         return null;
     }
-
 }
